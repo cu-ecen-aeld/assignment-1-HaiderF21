cc516801d87073840b59c91d501213e7938a14a2
HaiderF21 <f219409@cfd.nu.edu.pk>
2025-01-08 09:25:14 +0000

    Assignment-1 Completed and Passed the Test

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then
-echo "Error: Please enter minimum 2 Args " 
-echo "Usage: <Directory List> <search String>"
-exit 1
+if [ "$#" -ne 2 ]; then
+   echo "Error: Please enter minimum 2 Args " 
+   echo "Usage: <Directory List> <search String>"
+   exit 1
 fi
 
 filesdir=$1
 searchstr=$2
 
 if [ ! -d "$filesdir" ]; then
-echo "Error : Please Enter Valid Directory Path"
-exit 1
+   echo "Error : Please Enter Valid Directory Path"
+   exit 1
 fi
-echo $filesdir
+
+
 X=$(find "$filesdir" -type f | wc -l)
-Y=$(grep -sr "$searchstr" "$filesdir" | wc -l)
+Y=$(grep -r "$searchstr" "$filesdir" 2>/dev/null | wc -l)
 
-echo "Number of files = $X and Matched Lines $Y"
+echo "The number of files are $X and the number of matching lines are $Y"
+
 exit 0
-
 
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -2,22 +2,30 @@
 
 
 if [ "$#" -ne 2 ]; then
-echo "Error: Please Enter all the Arguments"
-echo "Usage: path/to/file/name StringWritten" 
-exit 1
+   echo "Error: Please Enter all the Arguments"
+   echo "Usage: path/to/file/name StringWritten" 
+   exit 1
 fi
 
 writefile=$1
 writestr=$2
 
 dir_Path=$(dirname "$writefile")
+if [ ! -d "$dir_Path" ]; then
+   echo "directory Doesnot Exist"
+   mkdir -p "$dir_Path"
+   echo "Directory created"
+    if [ "$?" -ne 0 ]; then
+        echo "failed to create $dir_Path"
+        exit 1
+    fi 
+fi
 
-mkdir -p "dir_Path"
-
-if echo "$writestr" > "$writefile"; then
-echo "Data has been Written Successfully"
-exit 0
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then
+  echo "Error : Writing to File"
+  exit 1
 else
-echo "Error: Couldnot write File $writefile"
-exit 1
-fi 
+  echo "Data Successfully written to File"
+fi
+exit 0
